f93b0d52fd237e6f9e8e55b19e31528811e1d454
Naren <narendra.m.savade@gmail.com>
2023-08-31 11:19:55 +0530

    31-Aug-2023 Data Ingestion Completed

---
+++ src/config/configuration.py
@@ -0,0 +1,29 @@
+from src.constants import *
+import os, sys
+
+
+# Creating folders to store all the data in it
+ROOT_DIR = ROOT_DIR_KEY
+
+# Dataset_url = 
+
+DATASET_PATH = os.path.join(ROOT_DIR, DATA_DIR, DATA_DIR_KEY)
+# This is the Dataset path C:\Users\Naren\source\repos\OneDrive\Desktop\GitHub\Ml-Project\src\config\Data\data.csv
+
+print(DATASET_PATH)
+RAW_FILE_PATH = os.path.join(ROOT_DIR, ARTIFACT_DIR_KEY, DATA_INGESTION_KEY, CURRENT_TIME_STAMP, 
+                             DATA_INGESTION_RAW_DATA_DIR, RAW_DATA_DIR_KEY)
+# It will create a path like this for raw data C:\Users\Naren\source\repos\OneDrive\Desktop\GitHub\Ml-Project\src\config\Artifact\data_ingestion\2023-08-31 10-36-51\raw_data_dir\raw.csv
+
+
+
+TRAIN_FILE_PATH = os.path.join(ROOT_DIR, ARTIFACT_DIR_KEY, DATA_INGESTION_KEY, CURRENT_TIME_STAMP,
+                               DATA_INGESTION_INGESTED_DATA_DIR_KEY, TRAIN_DATA_DIR_KEY)
+
+# It will create a train.csv file to store train dataset C:\Users\Naren\source\repos\OneDrive\Desktop\GitHub\Ml-Project\src\config\Artifact\data_ingestion\2023-08-31 10-38-18\ingested_dir\train.csv
+
+
+TEST_FILE_PATH = os.path.join(ROOT_DIR, ARTIFACT_DIR_KEY, DATA_INGESTION_KEY, CURRENT_TIME_STAMP,
+                               DATA_INGESTION_INGESTED_DATA_DIR_KEY, TEST_DATA_DIR_KEY)
+# It will create a test.csv file to store test dataset C:\Users\Naren\source\repos\OneDrive\Desktop\GitHub\Ml-Project\src\config\Artifact\data_ingestion\2023-08-31 10-38-18\ingested_dir\test.csv
+
---
+++ src/constants/__init__.py
@@ -0,0 +1,45 @@
+# This file is used to declare all the constant variable to be used in code to create paths and file names.
+
+import os, sys
+from datetime import datetime
+
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}"
+
+CURRENT_TIME_STAMP = get_current_time_stamp()
+
+ROOT_DIR_KEY = os.getcwd() #Store the path of Current working directory
+
+DATA_DIR ="Data" # Directory in which csv file is present
+DATA_DIR_KEY = "data.csv" #Actual file
+
+# Creating new Directory to store the downloaded data in client side
+
+ARTIFACT_DIR_KEY = "Artifact" #Declaring a Directory in which the downloaded data will be stored 
+
+
+# Data ingestion related variable used in Ingesting a data in the CLient side
+#  all are variables 
+
+DATA_INGESTION_KEY ="data_ingestion"  
+DATA_INGESTION_RAW_DATA_DIR = "raw_data_dir"
+DATA_INGESTION_INGESTED_DATA_DIR_KEY = "ingested_dir"
+RAW_DATA_DIR_KEY = "raw.csv"
+TRAIN_DATA_DIR_KEY = "train.csv"
+TEST_DATA_DIR_KEY = "test.csv"
+
+# It will create a directory like this 
+# Artifact/data_ingestion/Date_time_name_folder/Ingested_dir->train,test data set
+#                                                           -> raw data set 
+
+
+# Data Transformation related variable
+
+# DATA_TRANSFORMATION_ARTIFACT = "data_transformation"
+# DATA_PREPROCESSED_DIR = "processor"
+# DATA_TRANSFORMATION_PROCESSING_OBJ = "processor.pkl"
+# DATA_TRANSFORM_DIR = "transformation"
+# TRANSFORM_TRAIN_DIR_KEY = "train.csv"
+# TRANSFORM_TEST_DIR_KEY = "test.csv"
+
+
